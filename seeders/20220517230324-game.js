3c87e3126bad6f0f29a002c9782e74432d355721
Rocky Rosales <rocky.j.rosales@gmail.com>
2022-05-17 18:56:57 -0500

    seeder

--- seeders/20220517230324-game.js
+++ seeders/20220517230324-game.js
@@ -3,26 +3,32 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     
-    await queryInterface.bulkInsert('gameInfo', [{
-      name: 'Bulbasaur',
-      hp: 50,
-      type: 'grass',
+    await queryInterface.bulkInsert('Games', [{
+      gamename: 'Pac-Man',
+      gameid: 1,
+      star: 5,
+      review: 'Still a classic!',
+      username: 'GamerDude',
       createdAt: new Date(),
       updatedAt: new Date()
     },
       {
-      name: 'Charmander',
-      hp: 40,
-      type: 'fire',
-      createdAt: new Date(),
-      updatedAt: new Date()
+        gamename: 'Sonic the Hedgehog',
+        gameid: 2,
+        star: 4,
+        review: 'Weeeeee!',
+        username: 'Waffles69',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-      name: 'Squirtle',
-      hp: 50,
-      type: 'water', 
-      createdAt: new Date(),
-      updatedAt: new Date()
+        gamename: 'Mortal Combat',
+        gameid: 3,
+        star: 3,
+        review: 'The Super Nintedo blood is GREEN.....',
+        username: 'Bartman',
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
 
   ], {});
--- seeders/20220517230355-favorites.js
+++ seeders/20220517230355-favorites.js
@@ -2,17 +2,38 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    
+    await queryInterface.bulkInsert('Games', [{
+      gamename: 'Q-bert',
+      gameid: 7,
+      star: 2,
+      review: "It's OK",
+      username: 'GamerDude',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+      {
+        gamename: "Battle Toad's",
+        gameid: 8,
+        star: 4,
+        review: 'Kicks Ass!',
+        username: 'Waffles69',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        gamename: 'Virtual Fighter',
+        gameid: 9,
+        star: 5,
+        review: 'Blocky Fun.',
+        username: 'Bartman',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
 
+  ], {});
+    
+  },
   async down (queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
